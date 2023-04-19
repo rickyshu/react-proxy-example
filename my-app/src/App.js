16a75225e01746af3f30561a66f21de9c2e2af8f
rickyshu <hoinleekk@naver.com>
2023-04-20 08:17:55 +0900

    feat:proxy 기능 적용

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,60 +1,76 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import BookTable from './components/BookTable';
-import DisplayBoard from './components/DisplayBoard';
-import CreateBook from './components/CreateBook';
-import { getAllBooks, createBook } from './services/BookService';
-import Footer from './components/Footer';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import BookTable from "./components/BookTable";
+import DisplayBoard from "./components/DisplayBoard";
+import CreateBook from "./components/CreateBook";
+import { getAllBooks, createBook } from "./services/BookService";
+import { getAllTodos, createTodo } from "./services/TodoService";
+import Footer from "./components/Footer";
 
-function App () {
-
+function App() {
+  const Category = {
+    book: "Get All Books",
+    todo: "Get All Todos",
+  };
   const [bookShelf, setBookShelf] = useState({});
   const [books, setBooks] = useState([]);
+  const [todos, setTodos] = useState([]);
   const [numberOfBooks, setNumberBooks] = useState(0);
-
+  const [numberOfTodos, setNumberOfTodos] = useState(0);
   const handleSubmit = () => {
-      createBook(bookShelf)
-        .then(() => {
-          setNumberBooks(numberOfBooks+1);
-      });
-  }
-
+    createBook(bookShelf).then(() => {
+      setNumberBooks(numberOfBooks + 1);
+    });
+  };
   const getAllBook = () => {
-    getAllBooks()
-      .then(data => {
-        setBooks(data);
-        setNumberBooks(data.length);
-      });
-  }
+    getAllBooks().then((data) => {
+      setBooks(data);
+      setNumberBooks(data.length);
+    });
+  };
+
+  const getAllTodo = () => {
+    getAllTodos().then((date) => {
+      setTodos(date);
+      setNumberOfTodos(date.length);
+    });
+  };
 
   const handleOnChangeForm = (e) => {
-      let inputData = bookShelf;
-      if (e.target.name === 'book') {
-        bookShelf.book = e.target.value;
-      } else if (e.target.name === 'category') {
-        bookShelf.category = e.target.value;
-      } else if (e.target.name === 'author') {
-        bookShelf.author = e.target.value;
-      }
-      setBookShelf(inputData);
-  }
+    let inputData = bookShelf;
+    if (e.target.name === "book") {
+      bookShelf.book = e.target.value;
+    } else if (e.target.name === "category") {
+      bookShelf.category = e.target.value;
+    } else if (e.target.name === "author") {
+      bookShelf.author = e.target.value;
+    }
+    setBookShelf(inputData);
+  };
 
-  
   return (
     <div className="main-wrapper">
       <div className="main">
         <Header />
-        <CreateBook 
+        <CreateBook
           bookShelf={bookShelf}
           onChangeForm={handleOnChangeForm}
           handleSubmit={handleSubmit}
         />
-        <DisplayBoard 
-          numberOfBooks={numberOfBooks} 
-          getAllBook={getAllBook} 
+        <DisplayBoard
+          Category={Category.book}
+          numberOfBooks={numberOfBooks}
+          getAllBook={getAllBook}
         />
         <BookTable books={books} />
+        <DisplayBoard
+          Category={Category.todo}
+          numberOfTodos={numberOfTodos}
+          getAllBook={getAllTodo}
+        />
+        <BookTable books={todos} />
+
         <Footer />
       </div>
     </div>
--- my-app/src/components/DisplayBoard.js
+++ my-app/src/components/DisplayBoard.js
@@ -1,20 +1,17 @@
-const DisplayBoard = ({numberOfBooks, getAllBook}) => {
-    
-    return(
-        <div className="display-wrapper">
-            <div className="display-box">
-                <div className="display-board">
-                    <h4>생성된 수</h4>
-                    <div className="number">
-                    {numberOfBooks}
-                    </div>
-                </div>
-                <div className="get-button">
-                    <button onClick={() => getAllBook()}>Get all Books</button>
-                </div>
-            </div>
+const DisplayBoard = ({ Category, numberOfBooks, getAllBook }) => {
+  return (
+    <div className="display-wrapper">
+      <div className="display-box">
+        <div className="display-board">
+          <h4>생성된 수</h4>
+          <div className="number">{numberOfBooks}</div>
         </div>
-    )
-}
+        <div className="get-button">
+          <button onClick={() => getAllBook()}>{Category}</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DisplayBoard;
